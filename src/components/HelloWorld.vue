50cd44da44adea24f2dae74ca705b1d7ee503fa1
Adrian Karlsen <54044745+adrkaar@users.noreply.github.com>
2022-06-11 15:59:48 +0200

    master to gh-pages

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,22 @@
 <template>
   <v-container>
-    <v-row class="text-center"> </v-row>
+    <v-row>
+      <v-col>
+        <v-img
+          src="https://scontent.fosl4-1.fna.fbcdn.net/v/t31.18172-8/27798207_10155258150000857_9059556998327436468_o.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_aid=0&_nc_ohc=0GdQzIi1gvoAX9Xi4OR&_nc_ht=scontent.fosl4-1.fna&oh=00_AT8KaOlSnKPZ-XomqyMwBMOyrd-SJscWYTvVAFEG7ITInQ&oe=62CA50F8"
+          transition="scale-transition"
+          max-width="300"
+          max-height="300"
+          class="pa-4 secondary text-no-wrap rounded-pill"
+        >
+        </v-img>
+        <h2>Vil du suge en fjært ruben?</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-btn>Ja</v-btn>
+      <v-btn>Nei</v-btn>
+    </v-row>
   </v-container>
 </template>
 
