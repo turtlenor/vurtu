1f65516856ec608d8035ff353b942a49db042664
Adrian Karlsen <54044745+adrkaar@users.noreply.github.com>
2022-06-11 17:42:22 +0200

    Edvard sin tur

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-container>
+  <v-container class="align-center">
     <v-row>
       <v-col>
         <v-img
-          src="https://scontent.fosl4-1.fna.fbcdn.net/v/t31.18172-8/27798207_10155258150000857_9059556998327436468_o.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_aid=0&_nc_ohc=0GdQzIi1gvoAX9Xi4OR&_nc_ht=scontent.fosl4-1.fna&oh=00_AT8KaOlSnKPZ-XomqyMwBMOyrd-SJscWYTvVAFEG7ITInQ&oe=62CA50F8"
+          src="https://scontent.fosl4-2.fna.fbcdn.net/v/t1.6435-9/39056495_10214340028907082_2163652468997619712_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=t6j-mbBjNcUAX99ok2g&tn=97WA1bD08EkRTirD&_nc_ht=scontent.fosl4-2.fna&oh=00_AT9M7WSIGTfNZ34CpgKfTYjHVaeej06jh1a-zrxGcTw88Q&oe=62C9ABA9"
           transition="scale-transition"
           max-width="300"
           max-height="300"
           class="pa-4 secondary text-no-wrap rounded-pill"
         >
         </v-img>
-        <h2>Vil du suge en fjært ruben?</h2>
+        <h2>Vil du suge en fjært Edvard?</h2>
       </v-col>
     </v-row>
     <v-row>
-      <v-btn>Ja</v-btn>
+      <v-btn class="clickFart">Ja</v-btn>
       <v-btn>Nei</v-btn>
     </v-row>
   </v-container>
